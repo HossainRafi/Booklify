4c070ce4447cb4037f40a889362f7280b52f5a26
HossainRafi <hossainrafi71@gmail.com>
2022-11-26 23:56:09 +0600

    book details structure done

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import "./BookDetails.css";
+import { FaArrowLeft } from "react-icons/fa";
+
+const URL = "https://openlibrary.org/works/";
 
 const BookDetails = () => {
-  return (
-    <div>BookDetails</div>
-  )
-}
 
-export default BookDetails
+  return (
+    <section className="book-details">
+      <div className="container">
+        <button
+          type="button"
+          className="flex flex-c back-btn"
+        >
+          <FaArrowLeft size={22} />
+          <span className="fs-18 fw-6">Go Back</span>
+        </button>
+
+        <div className="book-details-content grid">
+          <div className="book-details-img">
+            <img src="" alt="cover img" />
+          </div>
+          <div className="book-details-info">
+            <div className="book-details-item title">
+              <span className="fw-6 fs-24"></span>
+            </div>
+            <div className="book-details-item description">
+              <span></span>
+            </div>
+            <div className="book-details-item">
+              <span className="fw-6">Subject Places: </span>
+              <span className="text-italic"></span>
+            </div>
+            <div className="book-details-item">
+              <span className="fw-6">Subject Times: </span>
+              <span className="text-italic"></span>
+            </div>
+            <div className="book-details-item">
+              <span className="fw-6">Subjects: </span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BookDetails;
