9465dc134f5a99121a6a23c21a20f9c9de593259
HossainRafi <hossainrafi71@gmail.com>
2022-11-26 21:46:41 +0600

    navbar structure done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Navbar.css";
+import logoImg from "../../images/logo.png";
+import { AiOutlineMenuFold } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleNavbar = () => setToggleMenu(!toggleMenu);
 
-export default Navbar
+  return (
+    <nav className="navbar" id="navbar">
+      <div className="container navbar-content flex">
+        <div className="brand-and-toggler flex flex-sb">
+          <Link to="/" className="navbar-brand flex">
+            <img src={logoImg} alt="site logo" />
+            <span className="fw-7 fs-24 ls-1">Booklify</span>
+          </Link>
+          <button
+            type="button"
+            className="navbar-toggler-btn"
+            onClick={handleNavbar}
+          >
+            <AiOutlineMenuFold
+              size={35}
+              style={{
+                color: `${toggleMenu ? "#fff" : "#010101"}`,
+              }}
+            />
+          </button>
+        </div>
+
+        <div
+          className={
+            toggleMenu
+              ? "navbar-collapse show-navbar-collapse"
+              : "navbar-collapse"
+          }
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                to="book"
+                className="nav-link text-uppercase text-white fs-22 fw-6 ls-1"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="about"
+                className="nav-link text-uppercase text-white fs-22 fw-6 ls-1"
+              >
+                about
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
