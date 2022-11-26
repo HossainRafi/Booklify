d4b7c1d1a2668a88c226a996a5b1c9abf89fcda9
HossainRafi <hossainrafi71@gmail.com>
2022-11-26 22:33:58 +0600

    search component structure

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import "./SearchForm.css";
 
 const SearchForm = () => {
-  return (
-    <div>SearchForm</div>
-  )
-}
 
-export default SearchForm
+  return (
+    <div className="search-form">
+      <div className="container">
+        <div className="search-form-content">
+          <form className="search-form">
+            <div className="search-form-elem flex flex-sb bg-white">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="The Lost World ..."
+              />
+              <button
+                type="submit"
+                className="flex flex-c"
+              >
+                <FaSearch className="text-purple" size={32} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchForm;
