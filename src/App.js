ad760f38fa71fb8d4bd33183220d55acf19a4705
HossainRafi <hossainrafi71@gmail.com>
2022-11-26 19:36:59 +0600

    react router dom setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h1>Hello Everyone</h1>
-      <h1>
-        This Project Contains The Basic Setup Of A New React Project. You Need
-        To Change Only The Title & Favicon Of This Project
-      </h1>
-    </div>
+    <>
+      <Routes></Routes>
+    </>
   );
 }
 
